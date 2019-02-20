92d26c7572a7409bf2476f51a0407c52e3580adb
Flavio Diez <flaviofdiez@gmail.com>
2019-02-20 21:45:51 +0100

    loading a list of books from json

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,41 @@
+import 'dart:convert';
+
+import 'package:amazeon/initial_values/books_json.dart';
+import 'package:amazeon/models/Book.dart';
 import 'package:amazeon/showcase.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  List<Book> books = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    books = _getBooks();
+
+    print(books.length);
+  }
+
+  _getBooks() {
+    var fileContents = booksJson;
+    List<Book> booksList = [];
+    for (Map map in jsonDecode(fileContents)) {
+      booksList.add(Book.fromJson(map));
+    }
+
+    return booksList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
